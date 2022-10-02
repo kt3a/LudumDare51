27a673bd0a841dcf76b05aa54b3008fe7d40d764
Katie Tooher <44885677+kt3a@users.noreply.github.com>
2022-10-01 23:54:15 -0500

    health

---
+++ Assets/Scripts/Character/HealthScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthScript : MonoBehaviour
+{
+    public static int totalhealth = 100;
+    public TextMeshProUGUI textmesh;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
