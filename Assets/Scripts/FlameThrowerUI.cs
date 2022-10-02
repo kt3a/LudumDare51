868b8a9381cb0e648a06a64a41fa9e9ed88bfdcc
Katie Tooher <44885677+kt3a@users.noreply.github.com>
2022-10-02 17:27:18 -0500

    flame thrower enabled

--- Assets/Scripts/FlameThrowerUI.cs
+++ Assets/Scripts/FlameThrowerUI.cs
@@ -7,7 +7,12 @@ public class FlameThrowerUI : MonoBehaviour
 {
     public static int total_parts = 0;
     public TextMeshProUGUI textmesh;
-
+    public static bool canflamethrow = false;
+    FlamethrowerEnabler flamethrower;
+    private void Start()
+    {
+        flamethrower = GameObject.FindObjectOfType<FlamethrowerEnabler>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,9 +20,11 @@ public class FlameThrowerUI : MonoBehaviour
         {
             textmesh.text = "Flame Thrower Parts: " + total_parts + " of 4";
         }
-        if (total_parts == 4)
+        if (total_parts == 4 && canflamethrow == false)
         {
-            textmesh.text = "You have the Flame Thrower.\nGo Find Big Mama.\nDefeat Big Mama";
+            textmesh.text = "You have the Flame Thrower.\nGo Find Big Mama.\nDefeat Big Mama.";
+            canflamethrow = true;
+            flamethrower.ToggleFlamethrower();
         }
     }
 }
